b94ab03f5875606d6b69a4e1f66045d34948acb5
Garik023 <garik.khachanyan@gmail.com>
2022-05-25 13:31:23 +0400

    new changes

--- src/components/chooseContract/ChooseContract.jsx
+++ src/components/chooseContract/ChooseContract.jsx
@@ -1,6 +1,7 @@
 import style from "../chooseContract/chooseContract.module.scss";
-import lock from "../../images/icons/klinikos.svg";
+import lock from "../../images/icons/krepselis.svg";
 import file from "../../images/icons/saskaitas.svg";
+import Radio from "../custom/radio/Radio";
 
 export default function ChooseContract() {
   return (
@@ -8,43 +9,47 @@ export default function ChooseContract() {
       <div className={style.buyContent}>
         <ul>
           <li>
-            <div>
-              <input type="radio" />
-            </div>
-            <div>
-              <p>
-                <b>Neterminuota sutartis</b>
-              </p>
-              <p>kaina menesiui</p>
-            </div>
-            <div>
-              <span>
-                <b>15,00 €</b>
-              </span>
-              <span></span>
-            </div>
+            <Radio name={"choose__content"} id={"choose__content-1"} value={1}>
+              <div>
+                <p>
+                  <b>Neterminuota sutartis</b>
+                </p>
+                <p>kaina menesiui</p>
+              </div>
+              <div>
+                <span>
+                  <b>15,00 €</b>
+                </span>
+                <span></span>
+              </div>
+            </Radio>
           </li>
           <li>
-            <div>
-              <input type="radio" />
-            </div>
-            <div>
-              <p>
-                <b>Metine sutartis</b>
-              </p>
-              <p>kaina menesiui</p>
-            </div>
-            <div>
-              <span>
-                <b>15,00 €</b>
-              </span>
-              <span></span>
-            </div>
+            <Radio name={"choose__content"} id={"choose__content-2"} value={2}>
+              <div>
+                <p>
+                  <b>Metine sutartis</b>
+                </p>
+                <p>kaina menesiui</p>
+              </div>
+              <div>
+                <span>
+                  <b>15,00 €</b>
+                </span>
+                <span></span>
+              </div>
+            </Radio>
           </li>
         </ul>
         <div className={style.buttonWrapp}>
-            <a><img src={lock.src} alt="lock" /><span>Pirkti</span></a>
-            <a><img src={file.src} alt="" /><span>PDF pasiūlymas</span></a>
+          <a>
+            <img src={lock.src} alt="lock" />
+            <span>Pirkti</span>
+          </a>
+          <a>
+            <img src={file.src} alt="" />
+            <span>PDF pasiūlymas</span>
+          </a>
         </div>
       </div>
     </div>
