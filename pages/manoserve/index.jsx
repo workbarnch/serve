db6c824ec8be2189c5ff7b44c2c784bd8ec6021d
workbarnch <work.norayr@gmail.com>
2022-05-31 12:34:44 +0400

    new page

---
+++ pages/manoserve/index.jsx
@@ -0,0 +1,13 @@
+import PageTitle from "../../src/components/pagetitle/PageTitle";
+
+
+export default function ManoServe(){
+    return (
+        <div className='wrapper'>
+            <PageTitle title="Planai ir mokėjimo būdai" />
+            <div className='container'>
+
+            </div>
+        </div>
+    )
+}
