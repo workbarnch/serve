3269c7d48e6dd531bc06331ca1a1151168ea9a7f
Garik023 <garik.khachanyan@gmail.com>
2022-05-31 12:35:49 +0400

    new-modal

---
+++ src/components/payment _methods/HandServe.jsx
@@ -0,0 +1,9 @@
+import style from "../payment _methods/handServe.module.scss";
+
+export default function HandServe () {
+      return(
+          <div className={style.container}>
+                <div className={style.titleWrap}><h2>Mano planas</h2></div>
+          </div>
+      )
+}
